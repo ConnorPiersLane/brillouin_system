c8c3f99399fdd27189919bcfc3395ec9bdee95f5
Mandelstam Lab PC <Mandelstam@nowhere.local>
2025-06-11 19:56:37 -0400

    fixed small live bugs

--- src/brillouin_system/gui/brillouin_viewer/brillouin_viewer.py
+++ src/brillouin_system/gui/brillouin_viewer/brillouin_viewer.py
@@ -52,7 +52,7 @@ brillouin_manager = BrillouinManager(
 brillouin_manager = BrillouinManager(
         camera=IxonUltra(
             index = 0,
-            temperature = -80, #"off"
+            temperature = "off", #"off"
             fan_mode = "full",
             x_start = 40, x_end  = 120,
             y_start= 300, y_end  = 315,
