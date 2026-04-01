6896c27b3541783c084a107a3a1b57b6a12d6be8
Mandelstam Lab PC <Mandelstam@nowhere.local>
2026-04-01 13:57:34 -0400

    calibration now using bands

--- src/brillouin_system/calibration/config/calibration_config.py
+++ src/brillouin_system/calibration/config/calibration_config.py
@@ -17,14 +17,53 @@ class CalibrationConfig:
     reference: str  # "left", "right", or "distance"
     mode: str  # "poly" or "interp"
 
+    # @property
+    # def calibration_freqs(self) -> list[float]:
+    #     # compute frequencies on the fly
+    #     return [
+    #         round(self.start + i * self.step, 6)
+    #         for i in range(1000)
+    #         if self.start + i * self.step <= self.stop
+    #     ]
     @property
     def calibration_freqs(self) -> list[float]:
-        # compute frequencies on the fly
-        return [
-            round(self.start + i * self.step, 6)
-            for i in range(1000)
-            if self.start + i * self.step <= self.stop
-        ]
+        """
+        start = 4
+        stop = 6
+        step = 0.1
+        gives [4.0, 5.0,
+             4.1, 5.1,
+             4.2, 5.2,
+             ...
+             4.9, 5.9,
+             5.0]
+        Returns:
+
+        """
+        freqs = []
+
+        base = int(self.start)
+        bands = list(range(base, int(self.stop)))  # e.g., [4, 5]
+
+        i = 0
+        while True:
+            offset = i * self.step
+            added_any = False
+
+            for b in bands:
+                val = b + offset
+                if val < self.start:
+                    continue
+                if val <= self.stop:
+                    freqs.append(round(val, 6))
+                    added_any = True
+
+            if not added_any:
+                break
+
+            i += 1
+
+        return freqs
 
 
 def load_calibration_config(path: Path = CALIBRATION_TOML_PATH) -> CalibrationConfig:
